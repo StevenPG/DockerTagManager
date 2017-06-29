1853a282b2b577217b81b900a7b2ec7e9a2696b5
Steven Gantz <steven.p.gantz@gmail.com>
2017-06-29 18:32:27 -0400

    Added spring-boot-starter project into repo

---
+++ RegistryViewer/src/main/java/com/stevengantz/docker/RegistryViewerApplication.java
@@ -0,0 +1,12 @@
+package com.stevengantz.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistryViewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistryViewerApplication.class, args);
+	}
+}
