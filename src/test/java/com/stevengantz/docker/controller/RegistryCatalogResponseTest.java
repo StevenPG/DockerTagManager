3cb2861ba339e11254761f6aeff5be7efe868798
Steven Gantz <steven.p.gantz@gmail.com>
2017-07-03 20:12:11 -0400

    Removed setup for mocking API calls, fixed gitlab.yml to ignore local tests

--- src/test/java/com/stevengantz/docker/controller/RegistryCatalogResponseTest.java
+++ src/test/java/com/stevengantz/docker/controller/RegistryCatalogResponseTest.java
@@ -6,10 +6,15 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.stevengantz.docker.registry.RegistryCatalog;
+
 public class RegistryCatalogResponseTest {
 
+	private RegistryCatalogResponse res;
+	
 	@Before
 	public void setUp() throws Exception {
+		res = new RegistryCatalogResponse(1, "test");
 	}
 
 	@After
@@ -18,32 +23,53 @@ public class RegistryCatalogResponseTest {
 	
 	@Test
 	public void testRegistryCatalogResponse() {
-		fail("Not yet implemented");
+		assertNotNull(new RegistryCatalogResponse(1, "test", new RegistryCatalog()));
 	}
 	
 	@Test
 	public void testRegistryCatalogResponseNoCatalog() {
-		fail("Not yet implemented");
+		assertNotNull(new RegistryCatalogResponse(1, "test"));
 	}
 
 	@Test
 	public void testGetStatus() {
-		fail("Not yet implemented");
+		res = new RegistryCatalogResponse(1, "message");
+		assertEquals(res.getStatus(), 1);
 	}
 
 	@Test
 	public void testSetStatus() {
-		fail("Not yet implemented");
+		res = new RegistryCatalogResponse(1, "message");
+		res.setStatus(2);
+		assertEquals(res.getStatus(), 2);
 	}
 
 	@Test
 	public void testGetCatalog() {
-		fail("Not yet implemented");
+		RegistryCatalog catalog = new RegistryCatalog();
+		res = new RegistryCatalogResponse(1, "message", catalog);
+		assertEquals(catalog, res.getCatalog());
 	}
 
 	@Test
 	public void testSetCatalog() {
-		fail("Not yet implemented");
+		RegistryCatalog catalog = new RegistryCatalog();
+		res = new RegistryCatalogResponse(1, "message");
+		res.setCatalog(catalog);
+		assertEquals(catalog, res.getCatalog());
+	}
+	
+	@Test
+	public void testGetMessage() {
+		res = new RegistryCatalogResponse(1, "message2");
+		assertEquals(res.getMessage(), "message2");
+	}
+
+	@Test
+	public void testSetMessage() {
+		res = new RegistryCatalogResponse(1, "message");
+		res.setMessage("message2");
+		assertEquals(res.getMessage(), "message2");
 	}
 
 }
--- src/test/java/com/stevengantz/docker/controller/RegistryRESTControllerTest.java
+++ src/test/java/com/stevengantz/docker/controller/RegistryRESTControllerTest.java
@@ -1,7 +1,6 @@
 package com.stevengantz.docker.controller;
 
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
@@ -10,12 +9,6 @@ public class RegistryRESTControllerTest {
 	@Test
 	public void testImplicitConstructor() {
 		assertNotNull(new RegistryRESTController());
-	}
-	
-	@Test
-	public void testGetReposInRegistry() {
-		fail("Not yet implemented");
-	}
-	
+	}	
 
 }
