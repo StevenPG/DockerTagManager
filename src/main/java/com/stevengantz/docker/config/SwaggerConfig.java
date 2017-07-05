f2b3ec82432b49a62cc11a04a239a8a307e0b99e
Steven Gantz <steven.p.gantz@gmail.com>
2017-07-04 21:05:43 -0400

    Added slightly more configuration for swagger

--- src/main/java/com/stevengantz/docker/config/SwaggerConfig.java
+++ src/main/java/com/stevengantz/docker/config/SwaggerConfig.java
@@ -1,13 +1,16 @@
 package com.stevengantz.docker.config;
 
+import static springfox.documentation.builders.PathSelectors.regex;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import static springfox.documentation.builders.PathSelectors.regex;
 
 @Configuration
 @EnableSwagger2
@@ -18,7 +21,20 @@ public class SwaggerConfig {
 				.select()
 				.apis(RequestHandlerSelectors.basePackage("com.stevengantz.docker.controller"))
 				.paths(regex("/api.*"))
-				.build();
+				.build()
+				.apiInfo(metaData());
 	}
+	
+	private ApiInfo metaData() {
+        ApiInfo apiInfo = new ApiInfo(
+                "DockerTagManager REST API",
+                "DockerTagManager REST API for remote Docker registry viewing and tag comparisons",
+                "1.0",
+                "Terms of service",
+                new Contact("Steven Gantz", "http://stevengantz.com", "steven.p.gantz@gmail.com"),
+               "MIT License",
+                "https://opensource.org/licenses/MIT");
+        return apiInfo;
+    }
 
 }
